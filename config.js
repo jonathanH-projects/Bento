b99b933133629b8311ec71da3439db9b6e38c414
b88575jh <jonathan.hackett@student.manchester.ac.uk>
2022-11-01 17:14:32 +0000

    Added weather API key

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '4ddf75fc21cae6a94393c7bef5e2e016', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
