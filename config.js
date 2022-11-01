827fecb5a2d645b76ee8e7af8b8848d40520dbd3
b88575jh <jonathan.hackett@student.manchester.ac.uk>
2022-11-01 20:06:45 +0000

    Implemented gruvbox styling

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '4ddf75fc21cae6a94393c7bef5e2e016', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
